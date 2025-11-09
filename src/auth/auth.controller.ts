427ffea1fb39cea8e4784ba72edab68124a7a4f3
chaewon <cw06155@gmail.com>
2025-11-10 00:37:24 +0900

    회원가입 유효성 체크

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { AuthService } from './auth.service';
 import { User } from './auth.entity';
@@ -7,7 +13,8 @@ import { User } from './auth.entity';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post()
+  @Post('/signUp')
+  @UsePipes(ValidationPipe)
   signUp(@Body() authCredentialDto: AuthCredentialDto): Promise<User> {
     return this.authService.signUp(authCredentialDto);
   }
--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,9 +1,18 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsString, MinLength, MaxLength, Matches } from 'class-validator';
 
 export class AuthCredentialDto {
   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'password only accepts english and number'
+  })
   password: string;
 }
