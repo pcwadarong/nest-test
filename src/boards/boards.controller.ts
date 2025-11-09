c47ea368c5292d8fb333752240735ae281790b21
chaewon <cw06155@gmail.com>
2025-11-09 14:37:26 +0900

    게시물 생성하기 추후 dto를 쓰겠지만 탗입으로 만들었을 때 @body를 어떻게 사요하는지 두 가지 방법으로 작성함

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import type { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,5 +9,19 @@ export class BoardsController {
   @Get()
   getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  // @Post()
+  // createBoard(@Body() body: { title: string; description: string }) {
+  //   const { title, description } = body;
+  //   return this.boardService.createBoard(title, description);
+  // }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,17 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board = {
+      id: randomUUID(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
